938568c7af8fed1268def06117c5008f4bc13a90
HussainSyed268 <snaqvi.bscs21seecs@seecs.edu.pk>
2023-08-08 20:55:00 +0500

    setting up sys

---
+++ firebase-react-auth/src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArjPt-qusi8QDsGCkNioAVqcxKo3hFAQY",
+
+  authDomain: "react-firebase-auth-408c4.firebaseapp.com",
+
+  projectId: "react-firebase-auth-408c4",
+
+  storageBucket: "react-firebase-auth-408c4.appspot.com",
+
+  messagingSenderId: "923276814434",
+
+  appId: "1:923276814434:web:93eac1a330d4bf4989893b",
+
+  measurementId: "G-TBK0FYMYKT",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);

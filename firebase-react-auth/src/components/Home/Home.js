f7500d941d719a824d0ee535d9596309aafc0644
HussainSyed268 <snaqvi.bscs21seecs@seecs.edu.pk>
2023-08-12 18:55:51 +0500

    update home

--- firebase-react-auth/src/components/Home/Home.js
+++ firebase-react-auth/src/components/Home/Home.js
@@ -3,37 +3,40 @@ import { React, useState, useEffect } from "react";
 import Navbar from "./components/Navbar.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 import Sidebar from "./components/Sidebar.jsx";
+import { useNavigate } from "react-router-dom";
 const Home = (props) => {
+  const navigate = useNavigate();
   const [sidebarOpen, setSidebarOpen] = useState(false);
+
   useEffect(() => {
+    if (
+      props.user === "" ||
+      props.user === undefined ||
+      props.user === null ||
+      !props.user
+    ) {
+      // If user is not authenticated, navigate to the login page
+      navigate("/login");
+    }
+
     if (sidebarOpen) {
       // document.body.classList.add("overflow-hidden");
     } else {
       // document.body.classList.remove("overflow-hidden");
     }
-  }, [sidebarOpen]);
+  }, [props.user, sidebarOpen, navigate]);
 
   return (
     <div className="flex h-full">
-      {props.loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <Sidebar
-            name={props.user}
-            email={props.email}
-            sidebar={sidebarOpen}
-          />
-          <div>
-            <Navbar
-              name={props.user}
-              setSidebar={setSidebarOpen}
-              sidebar={sidebarOpen}
-            />
-            <Dashboard />
-          </div>
-        </>
-      )}
+      <Sidebar name={props.user} email={props.email} sidebar={sidebarOpen} />
+      <div>
+        <Navbar
+          name={props.user}
+          setSidebar={setSidebarOpen}
+          sidebar={sidebarOpen}
+        />
+        <Dashboard />
+      </div>
     </div>
   );
 };

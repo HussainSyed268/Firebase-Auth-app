010459e60d348621e3866f8ef026251e55ef2d53
HussainSyed268 <snaqvi.bscs21seecs@seecs.edu.pk>
2023-08-12 20:45:32 +0500

    api keys Hidden

--- firebase-react-auth/src/firebase.js
+++ firebase-react-auth/src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyArjPt-qusi8QDsGCkNioAVqcxKo3hFAQY",
-  authDomain: "react-firebase-auth-408c4.firebaseapp.com",
-  projectId: "react-firebase-auth-408c4",
-  storageBucket: "react-firebase-auth-408c4.appspot.com",
-  messagingSenderId: "923276814434",
-  appId: "1:923276814434:web:93eac1a330d4bf4989893b",
-  measurementId: "G-TBK0FYMYKT",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 
